7900519570cd07dca5381da67bb53b526106587a
Hinder23 <sxhinder@gmail.com>
2021-12-02 05:14:48 +0000

    fix: Revert to method of cumulative sum

--- src/chord.py
+++ src/chord.py
@@ -1,6 +1,31 @@
 from mido import MetaMessage, MidiFile, MidiTrack
 
 
+def accumulate_time(messages: list):
+    if len(messages) == 0:
+        return
+    current = 0
+    result = []
+    for msg, delta_time in messages:
+        current += delta_time
+        result.append((msg, current))
+    return result
+
+
+def decumulate_time(messages: list):
+    if len(messages) == 0:
+        return
+    result = []
+    for i in range(len(messages)):
+        if i == 0:
+            result.append(messages[0])
+        else:
+            msg, abs_time = messages[i]
+            _, prev_time = messages[i - 1]
+            result.append((msg, abs_time - prev_time))
+    return result
+
+
 def add_chord_markers(file: str, chords: list):
     """Add chord markers in given MIDI file and chord list.
     
@@ -8,25 +33,25 @@ def add_chord_markers(file: str, chords: list):
     """
     mid = MidiFile(file)
     track: MidiTrack = mid.tracks[1]
+    messages = [(msg, msg.time) for msg in track]
 
-    markers: list = []
+    # Calculate absolute time of each message
+    messages = accumulate_time(messages)
+
+    # Append chord markers into the track
     for name, offset in chords:
-        marker = MetaMessage("marker")
-        marker.text = name.replace("-", "b")
-        marker.time = 0
-        markers.append((marker, int(offset * mid.ticks_per_beat)))
+        marker_message = MetaMessage("marker")
+        marker_message.text = name.replace("-", "b")
+        abs_time = int(offset * mid.ticks_per_beat)
+        messages.append((marker_message, abs_time))
 
-    msg_index, marker_index = 0, 0
-    current_tick = 0
-    while msg_index < len(track) and marker_index < len(markers):
-        current_tick += track[msg_index].time
-        if markers[marker_index][1] <= current_tick:
-            if track[msg_index].type == "note_on":
-                track.insert(msg_index, markers[marker_index][0])
-            else:
-                track.insert(msg_index + 1, markers[marker_index][0])
-            msg_index += 1
-            marker_index += 1
-        msg_index += 1
+    # Sort all the messages, and decumulate to relative time
+    messages.sort(key=lambda x: x[1])
+    messages = decumulate_time(messages)
 
+    # Clear the track and add the processed messages
+    track.clear()
+    for msg, delta_time in messages:
+        msg.time = delta_time
+        track.append(msg)
     mid.save(file)
