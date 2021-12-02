c98add4e7cf61504ee368ae66d0f9c23728f002e
Hinder23 <sxhinder@gmail.com>
2021-12-02 03:57:23 +0000

    fix: Use two index to insert chord markers

--- src/chord.py
+++ src/chord.py
@@ -1,47 +1,28 @@
 from mido import MetaMessage, MidiFile, MidiTrack
 
 
-def accumulate_time(messages: list):
-    if len(messages) == 0:
-        return
-    current = 0
-    result = []
-    for msg, delta_time in messages:
-        current += delta_time
-        result.append((msg, current))
-    return result
-
-
-def deaccumulate_time(messages: list):
-    if len(messages) == 0:
-        return
-    result = []
-    for i in range(len(messages)):
-        if i == 0:
-            result.append(messages[0])
-        else:
-            msg, abs_time = messages[i]
-            _, prev_time = messages[i - 1]
-            result.append((msg, abs_time - prev_time))
-    return result
-
-
 def add_chord_markers(file: str, chords: list):
     mid = MidiFile(file)
     track: MidiTrack = mid.tracks[1]
-    messages = [(msg, msg.time) for msg in track]
 
-    messages = accumulate_time(messages)
+    markers: list = []
     for name, offset in chords:
-        marker_message = MetaMessage("marker")
-        marker_message.text = name.replace("-", "b")
-        abs_time = int(offset * mid.ticks_per_beat)
-        messages.append((marker_message, abs_time))
-    messages.sort(key=lambda x: x[1])
-    messages = deaccumulate_time(messages)
+        marker = MetaMessage("marker")
+        marker.text = name.replace("-", "b")
+        marker.time = 0
+        markers.append((marker, int(offset * mid.ticks_per_beat)))
 
-    track.clear()
-    for msg, delta_time in messages:
-        msg.time = delta_time
-        track.append(msg)
+    msg_index, marker_index = 0, 0
+    current_tick = 0
+    while msg_index < len(track) and marker_index < len(markers):
+        current_tick += track[msg_index].time
+        if markers[marker_index][1] <= current_tick:
+            if track[msg_index].type == "note_on":
+                track.insert(msg_index, markers[marker_index][0])
+            else:
+                track.insert(msg_index + 1, markers[marker_index][0])
+            msg_index += 1
+            marker_index += 1
+        msg_index += 1
+
     mid.save(file)
